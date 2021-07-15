ed4582959fc0783b3cd8bb9dd2ccc220ed11391c
Hossein Zafari <ho3einzafari@gmail.com>
2021-07-15 16:50:18 +0430

    add CarItem component and setup the project

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <CarItem />
+
+      <StatusBar style={'auto'}/>
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +22,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ 
---
+++ components/CarItem/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View , Text ,  ImageBackground } from 'react-native';
+import styles from './styles';
+
+const CarItem = (props) => {
+    return (
+        <View style={styles.carContainer}>
+                <ImageBackground style={styles.image} source={require('../../assets/images/ModelS.jpeg')}/>
+                <View style={styles.titles}>
+                <Text style={styles.title}>Model S</Text>
+                <Text style={styles.subTitle}>Starting at $69.420</Text>
+                </View>
+        </View> 
+    );
+}
+    
+    
+export default CarItem;
